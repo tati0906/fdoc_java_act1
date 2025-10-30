3595678757939cd63a741017ce98c09c332ce21d
Tatiana V <tatianavanegas2006@gmail.com>
2025-10-30 15:28:33 -0500

    solucion actividad completa

---
+++ actividad1/src/main/java/com/example/EjerciciosActividad1.java
@@ -0,0 +1,45 @@
+package com.example;
+
+public class EjerciciosActividad1 {
+    public static void main(String[] args) {
+        // --- Declaración de variables ---       
+        // TODO: Implementación en main
+
+        // --- Identificadores ---       
+        // TODO: Implementación en main
+
+        // --- Identificadores ---       
+        // TODO: Implementación en main
+
+        // --- Estilos de convención de codificación ---       
+        // TODO: Implementación en main
+
+        // --- Tipos de datos primitivos ---       
+        // TODO: Implementación en main
+
+        // --- Tipos de datos no primitivos ---       
+        // TODO: Implementación en main
+
+        // --- Operadores aritméticos ---       
+        // TODO: Implementación en main
+
+        // --- Operadores de asignación ---       
+        // TODO: Implementación en main
+
+        // --- Operadores de comparación ---       
+        // TODO: Implementación en main
+
+        // --- Operadores lógicos ---       
+        // TODO: Implementación en main
+
+        // --- If ---       
+        // TODO: Implementación en main
+
+        // --- Switch ---       
+        // TODO: Implementación en main
+
+        // --- Otros ejemplos ---       
+        // TODO: Implementación en main
+        
+    }
+}
